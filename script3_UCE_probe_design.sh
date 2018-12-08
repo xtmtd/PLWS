febe079394d6288d613c7473884b1788dd3b9266
xtmtd <45136134+xtmtd@users.noreply.github.com>
2018-12-08 13:29:17 +0800

    Update script3_UCE_probe_design.sh

--- script3_UCE_probe_design.sh
+++ script3_UCE_probe_design.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 #Prepare a species list file (species.list with base species Pediculus_humanus excluded) in the initial working folder
+#Copy all the genome assemblies to working_folder/assembly/ and replace assembly name using "species_name.fa"
 #Tools samtools, art, BBtools, faToTwoBit and stampy are used in this script and the former two have been installed in environmental paths
 
+
 #Define variables
-DIR_ASSEMBLY="/home/zf/test/phylogenomics/assembly"
-DIR_fa2bit="/home/zf/test/phylogenomics/uce/probe"
-DIR_STAMPY="/home/zf/stampy-1.0.32"
+DIR_ASSEMBLY="/home/zf/Desktop/scripts/test/uce/assembly"
+DIR_fa2bit="/usr/local/bin"
+DIR_STAMPY="/home/zf/install/stampy-1.0.32"
 SPECIES_NAME=$(cat species.list)
-base="Pediculus_humanus"
-GROUP="Phthiraptera"
+base="Pseudachorutes_palmiensis"
+GROUP="Collembola"
 THREADS="8"
+SUBSTITUTION_RATE="0.05" #for the species very divergent, rate value can be increased to 0.1
+SPECIFIC_COUNT="1"  #the maximum value is the "number of species" (base species excluded)
+FINAL_SPECIFIC_COUNT="1" #value is not less than "SPECIFIC_COUNT"
 
 #Initiate the phyluce environment
 source activate phyluce
 
 mkdir genomes && cd genomes/
 
-#Copy all the genome assemblies to genomes/ and replace assembly name using "species_name.fa"
-for SPECIES in $SPECIES_NAME; do cp $DIR_ASSEMBLY/$SPECIES/scaffolds_$SPECIES.fa ./; mv scaffolds_$SPECIES.fa $SPECIES.fa; done
+#Copy all the genome assemblies to genomes/
+cp $DIR_ASSEMBLY/*.fa ./
 
 #Generate a python script simplify_headers.py to simplify the sequence head
+echo "simplify the sequence head..."
 echo '#!/usr/bin/python' >> ../simplify_headers.py
 echo 'from Bio import SeqIO' >> ../simplify_headers.py
 for SPECIES in $base $SPECIES_NAME
@@ -43,18 +49,21 @@ rm *.fa
 for SPECIES in *; do mkdir ${SPECIES%.*}; mv $SPECIES ${SPECIES%.*}; done
 
 #Convert genomes to 2bit format
+echo "Convert genomes to 2bit format..."
 for SPECIES in *; do $DIR_fa2bit/faToTwoBit $SPECIES/$SPECIES.fasta $SPECIES/${SPECIES%.*}.2bit; done
 
 #Simulate interleaved reads from genome assemblies (base genome "Pediculus_humanus" excluded)
 cd .. && mkdir reads && cd reads/
 for SPECIES in $SPECIES_NAME
   do
+    echo "Simulate interleaved reads from genome assembly of $SPECIES..."
     art_illumina --paired --in ../genomes/$SPECIES/$SPECIES.fasta --out $SPECIES-pe100-reads --len 100 --fcov 2 --mflen 200 --sdev 50 -ir 0.0 -ir2 0.0 -dr 0.0 -dr2 0.0 -qs 100 -qs2 100 -na
     reformat.sh in1=$SPECIES-pe100-reads1.fq in2=$SPECIES-pe100-reads2.fq out=$SPECIES-pe100-reads.fq.gz 1>>simulation.log 2>&1
     rm $SPECIES-pe100-reads1.fq $SPECIES-pe100-reads2.fq
   done
 
 #Prepare base genome prior to alignment
+echo "Prepare base genome..."
 cd .. && mkdir base && cd base/
 cp ../genomes/$base/$base.fasta ./
 $DIR_STAMPY/stampy.py --species="$base" --assembly="$base" -G $base $base.fasta
@@ -68,13 +77,14 @@ for SPECIES in $SPECIES_NAME
     export reads=$SPECIES-pe100-reads.fq.gz
     mkdir $SPECIES
     cd $SPECIES
-    $DIR_STAMPY/stampy.py --maxbasequal 93 -g ../../base/$base -h ../../base/$base --substitutionrate=0.05 -t $THREADS --insertsize=200 -M ../../reads/$reads | samtools view -Sb -@ $THREADS - > $SPECIES-to-$base.bam
+    $DIR_STAMPY/stampy.py --maxbasequal 93 -g ../../base/$base -h ../../base/$base --substitutionrate="$SUBSTITUTION_RATE" -t $THREADS --insertsize=200 -M ../../reads/$reads | samtools view -Sb -@ $THREADS - > $SPECIES-to-$base.bam
     echo "mapping status of $SPECIES to the base genome" >> ../mapping.log
     samtools flagstat $SPECIES-to-$base.bam -@ $THREADS >> ../mapping.log
     cd ..
   done
 
 #Remove those unmapped reads and symlink all of the reduced BAM files
+echo "Remove those unmapped reads and symlink all of the reduced BAM files..."
 mkdir all
 for SPECIES in $SPECIES_NAME
   do
@@ -84,40 +94,49 @@ for SPECIES in $SPECIES_NAME
   done
 
 #Convert bam to sorted bed
+echo "Convert bam to sorted bed..."
 cd .. && mkdir bed && cd bed/
 for i in ../alignments/all/*.bam; do echo $i; bedtools bamtobed -i $i -bed12 > `basename $i`.bed; done
 for i in *.bed; do echo $i; bedtools sort -i $i > ${i%.*}.sort.bed; done
 
 #Merge overlapping or nearly-overlapping intervals
+echo "Merge overlapping intervals..."
 for i in *.bam.sort.bed; do echo $i; bedtools merge -i $i > ${i%.*}.merge.bed; done
 for i in *.bam.sort.merge.bed; do wc -l $i; done
 
 #Remove repetitive intervals
+echo "Remove repetitive intervals..."
 for i in *.sort.merge.bed; do phyluce_probe_strip_masked_loci_from_set --bed $i --twobit ../genomes/$base/$base.2bit --output ${i%.*}.strip.bed --filter-mask 0.25 --min-length 80; done
 
 #Generate a configure file for bed files
+echo "Generate a configure file for bed files..."
 echo '[beds]' >> ../bed-files.conf
 for SPECIES in $SPECIES_NAME; do echo "$SPECIES:$SPECIES-to-$base-MAPPING.bam.sort.merge.strip.bed" >> ../bed-files.conf; done
 
 #Determining shared conserved loci
+echo "Determining shared conserved loci..."
 phyluce_probe_get_multi_merge_table --conf ../bed-files.conf --base-taxon $base --output $GROUP-to-$base.sqlite
 phyluce_probe_query_multi_merge_table --db $GROUP-to-$base.sqlite --base-taxon $base
-phyluce_probe_query_multi_merge_table --db $GROUP-to-$base.sqlite --base-taxon $base --output $base+9.bed --specific-counts 9
+phyluce_probe_query_multi_merge_table --db $GROUP-to-$base.sqlite --base-taxon $base --output "$base"+"$SPECIFIC_COUNT".bed --specific-counts $SPECIFIC_COUNT
 
 #Extract FASTA sequence from base genome for temp bait design
-phyluce_probe_get_genome_sequences_from_bed --bed $base+9.bed --twobit ../genomes/$base/$base.2bit --buffer-to 160 --output $base+9.fasta
+echo "Extract FASTA sequence from base genome for temp bait design..."
+phyluce_probe_get_genome_sequences_from_bed --bed "$base"+"$SPECIFIC_COUNT".bed --twobit ../genomes/$base/$base.2bit --buffer-to 160 --output "$base"+"$SPECIFIC_COUNT".fasta
 
 #Design a temporary bait set from the base taxon
-phyluce_probe_get_tiled_probes --input $base+9.fasta --probe-prefix "uce-" --design $GROUP-v1 --designer zf --tiling-density 3 --two-probes --overlap middle --masking 0.25 --remove-gc --output $base+9.temp.probes
+echo "Design a temporary bait set from the base taxon..."
+phyluce_probe_get_tiled_probes --input "$base"+"$SPECIFIC_COUNT".fasta --probe-prefix "uce-" --design $GROUP-v1 --designer zf --tiling-density 3 --two-probes --overlap middle --masking 0.25 --remove-gc --output "$base"+"$SPECIFIC_COUNT".temp.probes
 
 #Remove duplicates from our temporary bait set
-phyluce_probe_easy_lastz --target $base+9.temp.probes --query $base+9.temp.probes --identity 50 --coverage 50 --output $base+9.temp.probes-TO-SELF-PROBES.lastz
-phyluce_probe_remove_duplicate_hits_from_probes_using_lastz --fasta $base+9.temp.probes --lastz $base+9.temp.probes-TO-SELF-PROBES.lastz --probe-prefix=uce-
+echo "Remove duplicates from the temporary bait set..."
+phyluce_probe_easy_lastz --target "$base"+"$SPECIFIC_COUNT".temp.probes --query "$base"+"$SPECIFIC_COUNT".temp.probes --identity 50 --coverage 50 --output "$base"+"$SPECIFIC_COUNT".temp.probes-TO-SELF-PROBES.lastz
+phyluce_probe_remove_duplicate_hits_from_probes_using_lastz --fasta "$base"+"$SPECIFIC_COUNT".temp.probes --lastz "$base"+"$SPECIFIC_COUNT".temp.probes-TO-SELF-PROBES.lastz --probe-prefix=uce-
 
 #Align duplicate-free temporary bait against exemplar genomes
+echo "Align duplicate-free temporary bait against exemplar genomes..."
 cd .. && mkdir -p probe_design/$GROUP-genome-lastz && cd probe_design/
 echo "$base" >> ../species.list
-phyluce_probe_run_multiple_lastzs_sqlite --probefile ../bed/$base+9.temp-DUPE-SCREENED.probes --scaffoldlist $(cat ../species.list) --genome-base-path ../genomes --identity 50 --cores $THREADS --db $base+9+Osborniella.sqlite --output $GROUP-genome-lastz
+phyluce_probe_run_multiple_lastzs_sqlite --probefile ../bed/"$base"+"$SPECIFIC_COUNT".temp-DUPE-SCREENED.probes --scaffoldlist $(cat ../species.list) --genome-base-path ../genomes --identity 50 --cores $THREADS --db "$base"+"$SPECIFIC_COUNT".sqlite --output $GROUP-genome-lastz
 
 #Generate the configure file of the genome data
 echo '[scaffolds]' >> ../$GROUP-genome.conf
@@ -125,23 +144,30 @@ for SPECIES in $SPECIES_NAME; do echo "$SPECIES:../genomes/$SPECIES/$SPECIES.2bi
 echo "$base:../genomes/$base/$base.2bit" >> ../$GROUP-genome.conf
 
 #Extract sequence around conserved loci from exemplar genomes
-phyluce_probe_slice_sequence_from_genomes --conf ../$GROUP-genome.conf --lastz $GROUP-genome-lastz --probes 180 --name-pattern "$base+9.temp-DUPE-SCREENED.probes_v_{}.lastz.clean" --output $GROUP-genome-fasta
+echo "Extract sequence around conserved loci from exemplar genomes..."
+phyluce_probe_slice_sequence_from_genomes --conf ../$GROUP-genome.conf --lastz $GROUP-genome-lastz --probes 180 --name-pattern ""$base"+"$SPECIFIC_COUNT".temp-DUPE-SCREENED.probes_v_{}.lastz.clean" --output $GROUP-genome-fasta
 
 #Detect loci consistently across all of the exemplar taxa
+echo "Detect loci consistently across all of the exemplar taxa..."
 phyluce_probe_get_multi_fasta_table --fastas $GROUP-genome-fasta --output multifastas.sqlite --base-taxon $base
 
 #Get the distribution of hits among exemplar taxa
+echo "Get the distribution of hits among exemplar taxa..."
 phyluce_probe_query_multi_fasta_table --db multifastas.sqlite --base-taxon $base
 
 #Determine how many loci will be kept in final probe
-phyluce_probe_query_multi_fasta_table --db multifastas.sqlite --base-taxon $base --output $base+9-back-to-10.conf --specific-counts 10
+echo "Determine the number of loci kept in final probe..."
+phyluce_probe_query_multi_fasta_table --db multifastas.sqlite --base-taxon $base --output "$base"+"$SPECIFIC_COUNT"-back-to-"$FINAL_SPECIFIC_COUNT".conf --specific-counts "$FINAL_SPECIFIC_COUNT"
 
 #Design the final bait set
-phyluce_probe_get_tiled_probe_from_multiple_inputs --fastas $GROUP-genome-fasta --multi-fasta-output $base+9-back-to-10.conf --probe-prefix "uce-" --designer zf --design $GROUP-v1 --tiling-density 3 --overlap middle --masking 0.25 --remove-gc --two-probes --output $GROUP-v1-master-probe-list.fasta
+echo "Design the final bait set..."
+phyluce_probe_get_tiled_probe_from_multiple_inputs --fastas $GROUP-genome-fasta --multi-fasta-output "$base"+"$SPECIFIC_COUNT"-back-to-"$FINAL_SPECIFIC_COUNT".conf --probe-prefix "uce-" --designer zf --design $GROUP-v1 --tiling-density 3 --overlap middle --masking 0.25 --remove-gc --two-probes --output $GROUP-v1-master-probe-list.fasta
 
 #Remove duplicates from bait set
+echo "Remove duplicates from bait set..."
 phyluce_probe_easy_lastz --target $GROUP-v1-master-probe-list.fasta --query $GROUP-v1-master-probe-list.fasta --identity 50 --coverage 50 --output $GROUP-v1-master-probe-list-TO-SELF-PROBES.lastz
 phyluce_probe_remove_duplicate_hits_from_probes_using_lastz --fasta $GROUP-v1-master-probe-list.fasta --lastz $GROUP-v1-master-probe-list-TO-SELF-PROBES.lastz --probe-prefix=uce-
 
-source deactivate
+echo "The final bait set is /probe_design/"$GROUP"-v1-master-probe-list-DUPE-SCREENED.fasta"
 
+source deactivate
